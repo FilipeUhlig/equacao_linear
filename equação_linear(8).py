79f8aee13a7a5f2070d6b750935ee35319dcb6f6
FilipeUhlig <136847439+FilipeUhlig@users.noreply.github.com>
2023-06-24 10:12:55 -0300

    Add files via upload

---
+++ equação_linear(8).py
@@ -0,0 +1,19 @@
+"""
+Programa: Equação Linear
+Descrição: Este programa resolve uma equação linear do tipo ax = b
+Autor: Filipe
+Data: 04/06/2023
+Versão: 0.0.1
+"""
+#Atribuição de variáveis
+
+#Entrada de dados
+a = int(input("Qual é o valor de a, dado ax = b? "))
+b = int(input("Qual é o valor de b, dado ax = b? "))
+
+
+#Processamento de dados
+x = (b/a)
+
+#Saida de dados
+print(f"O valor de x é {x}.")
